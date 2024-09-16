610e75272563e24ff95d446005d20e4961f33737
Shailendra Kumar Saini <sainishailendra1996@gmail.com>
2024-09-16 13:03:29 +0530

    feat: resume url updated

--- src/app/profile/profile.service.ts
+++ src/app/profile/profile.service.ts
@@ -117,7 +117,7 @@ export class ProfileService {
   Extensive experience in Visual studio code IDE .
   Good knowledge of Regression and Cross Browser testing on various types of devices`
 
-  resumeurl = "https://drive.google.com/file/d/1WJMkdECykGz2FPidlFrl_6dF4ujy6FLx/view?usp=sharing"
+  resumeurl = "https://drive.google.com/file/d/1o3KrRaG9WIunL3Z9WAjh4EaBXwok3YVu/view?usp=sharing"
 
   skillsData: any = [
     {
